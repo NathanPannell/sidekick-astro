ef4421e843c7b28e8952631e99f428fa0ddf5b43
Nathan Pannell <n8tewolf@gmail.com>
2025-09-25 17:58:53 -0700

    chore: update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import playformInline from "@playform/inline";
-
-import mdx from "@astrojs/mdx";
-
 import alpinejs from "@astrojs/alpinejs";
 
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-	site: "https://pawstronaut.netlify.app",
+	site: "https://sidekickplayers.ca",
 	base: "/",
 	integrations: [
 		tailwind(),
-		alpinejs(),
-		mdx(),
-		(await import("@playform/inline")).default({
-			Critters: true,
-		}),
+		alpinejs()
 	],
 	output: "static",
 	devToolbar: {
 		enabled: false,
-	},
-	experimental: {
-		svg: true,
 	},
 });
